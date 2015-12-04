ba062648fe4c58260376af1f43005f89f6da2370
Rheinard Korf <rheinard@thekorfs.com>
2015-12-04 15:22:41 +1100

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,131 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+vagrant_dir = File.expand_path(File.dirname(__FILE__))
+
+Vagrant.configure("2") do |config|
+
+  # Store the current version of Vagrant for use in conditionals when dealing
+  # with possible backward compatible issues.
+  vagrant_version = Vagrant::VERSION.sub(/^v/, '')
+
+  # Configurations from 1.0.x can be placed in Vagrant 1.1.x specs like the following.
+  config.vm.provider :virtualbox do |v|
+    v.customize ["modifyvm", :id, "--memory", 1024]
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  end
+
+  # Forward Agent
+  #
+  # Enable agent forwarding on vagrant ssh commands. This allows you to use ssh keys
+  # on your host machine inside the guest. See the manual for `ssh-add`.
+  config.ssh.forward_agent = true
+
+  # Default Ubuntu Box
+  #
+  # This box is provided by Ubuntu vagrantcloud.com and is a nicely sized (332MB)
+  # box containing the Ubuntu 14.04 Trusty 64 bit release. Once this box is downloaded
+  # to your host computer, it is cached for future use under the specified box name.
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.hostname = "swiftbox"
+
+  # Default Box IP Address
+  #
+  # This is the IP address that your host will communicate to the guest through. In the
+  # case of the default `192.168.50.4` that we've provided, VirtualBox will setup another
+  # network adapter on your host machine with the IP `192.168.50.1` as a gateway.
+  #
+  # If you are already on a network using the 192.168.50.x subnet, this should be changed.
+  # If you are running more than one VM through VirtualBox, different subnets should be used
+  # for those as well. This includes other Vagrant boxes.
+  config.vm.network :private_network, ip: "192.168.100.4"
+
+  # External IP Address (example)
+  #
+  # To enable outside access to the virtual machine, a line similar to the following is
+  # required. Look for the IP address and adapter name in VirtualBox or by running
+  # `vboxmanage list bridgedifs` in a terminal on the host system. The common adapter name
+  # in OSX is `en0: Wi-Fi (AirPort)`. You will likely find a variety similar to the example
+  # below on Windows hosts.
+  #
+  config.vm.network :public_network, :bridge => 'en0: Wi-Fi (AirPort)', ip: '192.168.2.101'
+
+  # Drive mapping
+  #
+  # The following config.vm.synced_folder settings will map directories in your Vagrant
+  # virtual machine to directories on your local machine. Once these are mapped, any
+  # changes made to the files in these directories will affect both the local and virtual
+  # machine versions. Think of it as two different ways to access the same file. When the
+  # virtual machine is destroyed with `vagrant destroy`, your files will remain in your local
+  # environment.
+
+  # /srv/projects/
+  #
+  # If a projects directory exists in the same directory as your Vagrantfile,
+  # a mapped directory inside the VM will be created that contains these files.
+  config.vm.synced_folder "projects/", "/srv/projects"
+
+  # Customfile - POSSIBLY UNSTABLE
+  #
+  # Use this to insert your own (and possibly rewrite) Vagrant config lines. Helpful
+  # for mapping additional drives. If a file 'Customfile' exists in the same directory
+  # as this Vagrantfile, it will be evaluated as ruby inline as it loads.
+  #
+  # Note that if you find yourself using a Customfile for anything crazy or specifying
+  # different provisioning, then you may want to consider a new Vagrantfile entirely.
+  if File.exists?(File.join(vagrant_dir,'Customfile')) then
+    eval(IO.read(File.join(vagrant_dir,'Customfile')), binding)
+  end
+
+  # Provisioning
+  #
+  # Process one or more provisioning scripts depending on the existence of custom files.
+  #
+  # provison-pre.sh acts as a pre-hook to our default provisioning script. Anything that
+  # should run before the shell commands laid out in provision.sh (or your provision-custom.sh
+  # file) should go in this script. If it does not exist, no extra provisioning will run.
+  if File.exists?(File.join(vagrant_dir,'provision','provision-pre.sh')) then
+    config.vm.provision :shell, :path => File.join( "provision", "provision-pre.sh" )
+  end
+
+  # provision.sh or provision-custom.sh
+  #
+  # By default, Vagrantfile is set to use the provision.sh bash script located in the
+  # provision directory. If it is detected that a provision-custom.sh script has been
+  # created, that is run as a replacement. This is an opportunity to replace the entirety
+  # of the provisioning provided by default.
+  if File.exists?(File.join(vagrant_dir,'provision','provision-custom.sh')) then
+    config.vm.provision :shell, :path => File.join( "provision", "provision-custom.sh" )
+  else
+    config.vm.provision :shell, :path => File.join( "provision", "provision.sh" )
+  end
+
+  # provision-post.sh acts as a post-hook to the default provisioning. Anything that should
+  # run after the shell commands laid out in provision.sh or provision-custom.sh should be
+  # put into this file. This provides a good opportunity to install additional packages
+  # without having to replace the entire default provisioning script.
+  if File.exists?(File.join(vagrant_dir,'provision','provision-post.sh')) then
+    config.vm.provision :shell, :path => File.join( "provision", "provision-post.sh" )
+  end
+
+  # Vagrant Triggers
+  #
+  # If the vagrant-triggers plugin is installed, we can run various scripts on Vagrant
+  # state changes like `vagrant up`, `vagrant halt`, `vagrant suspend`, and `vagrant destroy`
+  #
+  # These scripts are run on the host machine, so we use `vagrant ssh` to tunnel back
+  # into the VM and execute things.
+  if defined? VagrantPlugins::Triggers
+    config.trigger.before :halt, :stdout => true do
+      run "vagrant ssh -c 'vagrant_halt'"
+    end
+    config.trigger.before :suspend, :stdout => true do
+      run "vagrant ssh -c 'vagrant_suspend'"
+    end
+    config.trigger.before :destroy, :stdout => true do
+      run "vagrant ssh -c 'vagrant_destroy'"
+    end
+  end
+end
---
+++ provision/provision.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+#
+# provision.sh
+#
+# This file is specified in Vagrantfile and is loaded by Vagrant as the primary
+# provisioning script whenever the commands `vagrant up`, `vagrant provision`,
+# or `vagrant reload` are used. It provides all of the default packages and
+# configurations included with Varying Vagrant Vagrants.
+
+# By storing the date now, we can calculate the duration of provisioning at the
+# end of this script.
+start_seconds="$(date +%s)"
+
+# Network Detection
+#
+# Make an HTTP request to google.com to determine if outside access is available
+# to us. If 3 attempts with a timeout of 5 seconds are not successful, then we'll
+# skip a few things further in provisioning rather than create a bunch of errors.
+if [[ "$(wget --tries=3 --timeout=5 --spider http://google.com 2>&1 | grep 'connected')" ]]; then
+	echo "Network connection detected..."
+	ping_result="Connected"
+else
+	echo "Network connection not detected. Unable to reach google.com..."
+	ping_result="Not Connected"
+fi
+
+# PACKAGE INSTALLATION
+#
+# Build a bash array to pass all of the packages we want to install to a single
+# apt-get command. This avoids doing all the leg work each time a package is
+# set to install. It also allows us to easily comment out or add single
+# packages. We set the array as empty to begin with so that we can append
+# individual packages to it as required.
+apt_package_install_list=()
+
+# Start with a bash array containing all packages we want to install in the
+# virtual machine. We'll then loop through each of these and check individual
+# status before adding them to the apt_package_install_list array.
+apt_package_check_list=(
+
+	imagemagick
+	subversion
+	git-core
+	zip
+	unzip
+	ngrep
+	curl
+	make
+	vim
+	colordiff
+	postfix
+	clang
+	emacs
+	emacs-goodies-el
+
+	# ntp service to keep clock current
+	ntp
+
+	# Req'd for i18n tools
+	gettext
+
+	# Req'd for Webgrind
+	graphviz
+
+	# dos2unix
+	# Allows conversion of DOS style line endings to something we'll have less
+	# trouble with in Linux.
+	dos2unix
+
+	# nodejs for use by grunt
+	g++
+	nodejs
+)
+
+echo "Check for apt packages to install..."
+
+# Loop through each of our packages that should be installed on the system. If
+# not yet installed, it should be added to the array of packages to install.
+for pkg in "${apt_package_check_list[@]}"; do
+	package_version="$(dpkg -s $pkg 2>&1 | grep 'Version:' | cut -d " " -f 2)"
+	if [[ -n "${package_version}" ]]; then
+		space_count="$(expr 20 - "${#pkg}")" #11
+		pack_space_count="$(expr 30 - "${#package_version}")"
+		real_space="$(expr ${space_count} + ${pack_space_count} + ${#package_version})"
+		printf " * $pkg %${real_space}.${#package_version}s ${package_version}\n"
+	else
+		echo " *" $pkg [not installed]
+		apt_package_install_list+=($pkg)
+	fi
+done
+
+# Disable ipv6 as some ISPs/mail servers have problems with it
+echo "inet_protocols = ipv4" >> /etc/postfix/main.cf
+
+if [[ $ping_result == "Connected" ]]; then
+	# If there are any packages to be installed in the apt_package_list array,
+	# then we'll run `apt-get update` and then `apt-get install` to proceed.
+	if [[ ${#apt_package_install_list[@]} = 0 ]]; then
+		echo -e "No apt packages to install.\n"
+	else
+		# Before running `apt-get update`, we should add the public keys for
+		# the packages that we are installing from non standard sources via
+		# our appended apt source.list
+
+		# Retrieve the Swift signing key from swift.org
+		echo "Applying Swift signing key..."
+		wget -q -O - https://swift.org/keys/all-keys.asc | gpg --import -
+
+		# Apply the nodejs assigning key
+		echo "Applying nodejs signing key..."
+		apt-key adv --quiet --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C7917B12 2>&1 | grep "gpg:"
+		apt-key export C7917B12 | apt-key add -
+
+		# update all of the package references before installing anything
+		echo "Running apt-get update..."
+		apt-get update --assume-yes
+
+		# install required packages
+		echo "Installing apt-get packages..."
+		apt-get install --assume-yes ${apt_package_install_list[@]}
+
+		# Clean up apt caches
+		apt-get clean
+	fi
+
+	# Make sure we have the latest npm version
+	npm install -g npm
+
+	# ack-grep
+	#
+	# Install ack-rep directory from the version hosted at beyondgrep.com as the
+	# PPAs for Ubuntu Precise are not available yet.
+	if [[ -f /usr/bin/ack ]]; then
+		echo "ack-grep already installed"
+	else
+		echo "Installing ack-grep as ack"
+		curl -s http://beyondgrep.com/ack-2.04-single-file > /usr/bin/ack && chmod +x /usr/bin/ack
+	fi
+
+	# COMPOSER
+	#
+	# Install Composer if it is not yet available.
+	if [[ ! -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
+		echo "Installing Composer..."
+		curl -sS https://getcomposer.org/installer | php
+		chmod +x composer.phar
+		mv composer.phar /usr/local/bin/composer
+	fi
+
+	# Update both Composer and any global packages. Updates to Composer are direct from
+	# the master branch on its GitHub repository.
+	if [[ -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
+		echo "Updating Composer..."
+		COMPOSER_HOME=/usr/local/src/composer composer self-update
+		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/phpunit:4.3.*
+		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/php-invoker:1.1.*
+		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update mockery/mockery:0.9.*
+		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update d11wtq/boris:v1.0.8
+		COMPOSER_HOME=/usr/local/src/composer composer -q global config bin-dir /usr/local/bin
+		COMPOSER_HOME=/usr/local/src/composer composer global update
+	fi
+
+	# Grunt
+	#
+	# Install or Update Grunt based on current state.  Updates are direct
+	# from NPM
+	if [[ "$(grunt --version)" ]]; then
+		echo "Updating Grunt CLI"
+		npm update -g grunt-cli &>/dev/null
+		npm update -g grunt-sass &>/dev/null
+		npm update -g grunt-cssjanus &>/dev/null
+	else
+		echo "Installing Grunt CLI"
+		npm install -g grunt-cli &>/dev/null
+		npm install -g grunt-sass &>/dev/null
+		npm install -g grunt-cssjanus &>/dev/null
+	fi
+
+	# Graphviz
+	#
+	# Set up a symlink between the Graphviz path defined in the default Webgrind
+	# config and actual path.
+	echo "Adding graphviz symlink for Webgrind..."
+	ln -sf /usr/bin/dot /usr/local/bin/dot
+
+	echo "Downloading Swift..."
+	cd ~
+	curl 'https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz' > swift.tar.gz
+	echo "Extracting Swift..."
+	mkdir swift-tools
+	tar -zxvf swift.tar.gz -C ./swift-tools/ --strip-components=1
+	echo "Installing Swift..."
+	cd ~/swift-tools/usr/
+	sudo rsync --remove-source-files -rl ./ /usr/local/
+	cd ~
+	rm -rf swift-tools
+
+else
+	echo -e "\nNo network connection available, skipping package installation"
+fi
+
+end_seconds="$(date +%s)"
+echo "-----------------------------"
+echo "Provisioning complete in "$(expr $end_seconds - $start_seconds)" seconds"
+if [[ $ping_result == "Connected" ]]; then
+	echo "External network connection established, packages up to date."
+else
+	echo "No external network available. Package installation and maintenance skipped."
+fi
